60c0fe6fa01f1bec21a51f17eab875ecdefa7c23
jaesus <boffering@hotmail.com>
2023-09-13 14:51:41 +0300

    progression

--- frontend/main.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'https://esm.sh/preact'
-import App from './app.jsx'
-
-React.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
-)
---
+++ frontend/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import MessageComp from './components/Message'
+import './App.css'
+import Data from '../../data.json'
+const Username = Data.currentUser.username
+
+const App = () => {
+  const [messages, setMessages] = useState(Data.comments)
+
+  function getAuth(auth){
+    if (auth === Username) {
+      return true
+    }
+    return false
+  }
+
+  const MappedMessages = messages.map((msg) => {
+
+      let Replies = null
+      
+      if (msg.replies.length > 0) {
+        Replies = msg.replies.map(reply => <MessageComp
+            isAuthor={getAuth(reply.user.username)}
+            data={reply}
+            key={reply.id}
+          />
+        )
+      }
+
+      return(
+        <div className='MessageTree'>
+          <MessageComp 
+            isAuthor={getAuth(msg.user.username)} 
+            data={msg}
+            key={msg.id}
+          />
+          {
+            Replies && 
+            <div className='Replies'>
+              <div className='Divider'>
+                <div className='DividerCenter'/>
+              </div>
+              <div className='RepliesContainer'>
+                {Replies}
+              </div>
+            </div>
+          }
+        </div>
+      )
+    }
+  )
+
+  return (
+    <div className='Room'>
+      {MappedMessages}
+    </div>
+  )
+}
+
+export default App
