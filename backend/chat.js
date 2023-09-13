db5324b0bbeb99b58044fb4071a48747eafb3a77
MasterSparkInYourFace <stoner9lab@tutanota.com>
2023-09-13 09:18:44 +0300

    license (GPL-3)

--- backend/chat.js
+++ backend/chat.js
@@ -1,8 +1,22 @@
-// chat tree implementation
-// TODO: license info (GPL-3?)
+/*
+ * comments backend - chat tree implementation
+ * Copyright (C) 2023  Marisa
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-// or diedrate
-function rehydrate(up, m, i) {
+function rehydrate(up, m, i) { // or diedrate
 	const a = new Message(up, i,
 		new User(m.user.name, m.user.png),
 		m.votes, m.content);
