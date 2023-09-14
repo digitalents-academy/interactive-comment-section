3d4c04f47448e3b84c2d9990b3493f8cc5ff0597
Mikko Manninen <mannmik@digitalentshelsinki.internal>
2023-09-14 13:17:37 +0300

    make makecert.sh work on msysgit

--- backend/certscript/makecert.sh
+++ backend/certscript/makecert.sh
@@ -4,6 +4,8 @@
 # $3: root cert
 # $4: root cert key
 
+export MSYS_NO_PATHCONV=1
+
 SUBJ="/O=Gensokyo/CN=localhost"
 SAN="subjectAltName=DNS:localhost,IP:127.0.0.1"
 
@@ -11,6 +13,6 @@ openssl req -new -key "$1" -out "$2.csr" -sha256\
 	-subj "$SUBJ" -addext "$SAN"
 
 openssl x509 -req -in "$2.csr" -out "$2" -days 365 -sha256\
-	-CA "$3" -CAkey "$4" -CAcreateserial -extfile <(echo "$SAN")
+	-CA "$3" -CAkey "$4" -CAcreateserial -extfile - <<< "$SAN"
 openssl x509 -in "$2" -noout -text
-rm "$2.csr"
+rm "$2.csr" "${3%.*}.srl"
