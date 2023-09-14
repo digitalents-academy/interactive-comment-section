49444f8b497553edb757b940ecf58efbb3dbaeb1
jaesus <boffering@hotmail.com>
2023-09-14 14:53:08 +0300

    v

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import * as fs from "node:fs";
+//import * as fs from "node:fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
+	/*server: {
 		https: {
 			key: fs.readFileSync("../backend/cert/ckey.pem"),
 			cert: fs.readFileSync("../backend/cert/cert.pem")
 		}
-	}
+	}*/
 })
