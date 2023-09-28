a5e3f7bb5365d870fe9ce084dc41b8287af17924
sasu <sasu@realemail.com>
2023-09-28 13:46:12 +0300

    json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { PORT, KEY, CERT } from '../a.json'
 
 import * as fs from "node:fs";
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	server: {
+		port: PORT,
 		https: {
-			key: fs.readFileSync("../backend/cert/ckey.pem"),
-			cert: fs.readFileSync("../backend/cert/cert.pem")
+			key: fs.readFileSync(path.resolve('..', KEY)),
+			cert: fs.readFileSync(path.resolve('..', CERT))
 		}
 	}
 })
